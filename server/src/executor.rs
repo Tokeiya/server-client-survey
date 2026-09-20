e839a7c7d019eaa2c7f47977d55671f7a5b7911f
tokeiya3 <net_seed@hotmail.com>
2026-09-21 08:22:03 +0900

    Initialize workspace with `client` and `server` crates, add basic project setup files, and implement skeleton for `Executor`.

---
+++ server/src/executor.rs
@@ -0,0 +1,61 @@
+use std::collections::VecDeque;
+use std::sync::atomic::AtomicBool;
+use tokio::sync::mpsc::error::TryRecvError;
+use tokio::{
+	runtime::{Builder, Runtime},
+	sync::mpsc::{Receiver, Sender},
+	sync::oneshot,
+	task::{JoinError, JoinHandle},
+	time::{Duration, sleep},
+};
+
+pub enum Command {
+	Value(String),
+	Dump(Sender<usize>),
+}
+
+pub struct Executor {
+	runtime: Runtime,
+	task: JoinHandle<usize>,
+}
+
+impl Executor {
+	pub fn new(rx: Receiver<Command>) -> Self {
+		let runtime = Builder::new_multi_thread()
+			.worker_threads(1)
+			.enable_all()
+			.build()
+			.unwrap();
+
+		let task = runtime.spawn(Self::loop_proc(rx));
+
+		Self { runtime, task }
+	}
+
+	async fn proc(value: String, id: usize) {
+		println!("{id}:{value} proceeded");
+		sleep(Duration::from_millis(500)).await;
+	}
+
+	async fn receive_proc(
+		rx: &mut Receiver<Command>,
+		queue: VecDeque<(usize, String)>,
+		seed: usize,
+	) -> Result<usize, JoinError> {
+		todo!()
+	}
+
+	async fn loop_proc(mut rx: Receiver<Command>) -> usize {
+		let mut queue: VecDeque<(usize, String)> = VecDeque::new();
+		let mut cnt = 0usize;
+
+		loop {
+			todo!()
+		}
+	}
+
+	fn join(self) -> Result<usize, JoinError> {
+		let Self { runtime, task } = self;
+		runtime.block_on(task)
+	}
+}
